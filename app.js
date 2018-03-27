9ec9abc12e754e95089e4335d7e7252f16a72364
joshua-deans <joshua.deans21@gmail.com>
2018-03-26 23:15:34 -0700

    Update app

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 var express = require('express');
 const bodyParser = require('body-parser');
-var methodOverride = require("method-override");
+var flash = require('connect-flash');
 var externalFunc = require("./functions/index.js");
+var session = require('express-session');
 var app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+app.use(session({
+    secret: "Anything I want",
+    resave: false,
+    saveUnitialized: false
+}));
 
+// app.use(methodOverride("_method"));
+app.use(flash());
 
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-    res.render('index', {title: "Express"});
+    res.render('index', {error: []});
 });
 
 app.post('/', function(req, res){
     var name = req.body.actorName.trim();
     var nameSplit = name.split(" ");
     var urls = externalFunc.getUrl(nameSplit);
-    externalFunc.getWebData(urls, res);
+    externalFunc.getWebData(urls, req, res);
 });
-//
-// app.get('/:firstName-:lastName', function(req, res){
-//     res.render('graph', {title: "Express", firstName: firstName, lastName: firstName});
-// });
 
-var port = 8888;
+var port = process.env.PORT || 8888;
 
 app.listen(port, process.env.IP, function(){
     console.log("The Server has started! on port " + port);
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const regression = require('regression');
+const Promise = require('promise');
 var funcObj = {};
 
 funcObj.getUrl = function(name){
     for (var i = 0; i < name.length; i++){
         name[i] = name[i].toLowerCase();
+        if (name[i].includes("-")){
+            name[i].replace("-", "");
+        }
     }
     if (name[1] === "dicaprio"){
         name[1] = "di";
@@ -19,20 +23,20 @@ funcObj.getUrl = function(name){
 
     actorUrl1 = 'https://www.rottentomatoes.com/celebrity/' + actorUrl1;
     actorUrl2 = 'https://www.rottentomatoes.com/celebrity/' + actorUrl2;
+
+    name = name.join(" ");
+
     return [actorUrl1, actorUrl2, name];
 };
 
-funcObj.getWebData = function(urlArr, res, index){
+funcObj.getWebData = function(urlArr, req, res) {
     var url = urlArr[0];
     var name = urlArr[2];
-    // webscrape the first url
-    var attempt1 = webScrape(url, name, res);
-    url = urlArr[1];
-    var attempt2 = webScrape(url, name, res);
-    // return res.redirect("back");
+    // scrape the first url
+    webScrape(url, name, urlArr, req, res);
 };
 
-function webScrape(url, name, res){
+function webScrape(url, name, urlArr, req, res){
     var data = [];
     request(url, function(error, response, html){
         // First we'll check to make sure no errors occurred when making the request
@@ -40,44 +44,65 @@ function webScrape(url, name, res){
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
             var $ = cheerio.load(html);
             // Finally, we'll define the variables we're going to capture
-            var actorName;
-            var movieCount = 0;
+            var actorName = $('.celeb_name').children().text();
 
-            actorName = $('.celeb_name').children().text();
+            data = cheerioMethod($, data);
 
-            $('#filmographyTbl').filter(function(){
-                // Let's store the data we filter into a variable so we can easily see what's going on.
-                var searchData = $(this).children("tbody").children();
-                // go through each film and collect data
-                searchData.each(function(i, elem) {
-                    var movieObj = {};
-                    var score = $(this).children("td").children(".tMeterIcon").children(".tMeterScore").text();
-                    if (score && score !== "No Score Yet") {
-                        movieObj.y = Number(score.slice(0, -1));
-                        movieObj.title = $(this).children("td").children("a").text();
-                        movieObj.link =  "www.rottentomatoes.com" + $(this).children("td").children("a").attr('href');
-                        movieObj.year = $(this).children().eq(-1).text();
-                        movieCount++;
-                        data.push(movieObj);
-                    }
-                });
-                data = data.reverse();
-                for (var i = 0; i<movieCount; i++){
-                    data[i].x = i+1;
-                }
-            });
-            if (data[0]) {
+            // if data is not empty
+            if (data.length > 0) {
                 var linearData = getLinearRegression(data);
-                res.render('graph', {actorName: actorName, dataArr: data, linearData: linearData.points});
+                res.render('graph', {actorName: actorName, dataArr: data, linearData: linearData});
             }
             else {
-                return -1;
+                // in case of no data being returned
+                if (url === urlArr[1]){
+                    console.log(name + " was not found on Rotten Tomatoes");
+                    req.flash("error", "\"" + name + "\" was not found on Rotten Tomatoes");
+                    return res.render("index", {error: req.flash("error")});
+                }
+                else {
+                    webScrape(urlArr[1], name, urlArr, req, res);
+                }
             }
         }
         else {
-            return -1;
+            if (url === urlArr[1]){
+                console.log("An error occured when finding " + name);
+                req.flash("error", "An error occurred when finding " + name);
+                return res.render("index", {error: req.flash("error")});
+            }
+            else {
+                webScrape(urlArr[1], name, urlArr, req, res);
+            }
         }
     });
+}
+
+function cheerioMethod($, data){
+    $('#filmographyTbl').filter(function(){
+        // Let's store the data we filter into a variable so we can easily see what's going on.
+        var searchData = $(this).children("tbody").children();
+        // Go through each film and collect data
+        searchData.each(function(i, elem) {
+            var movieObj = {};
+            var score = $(this).children("td").children(".tMeterIcon").children(".tMeterScore").text();
+            if (score && score !== "No Score Yet") {
+                movieObj.y = Number(score.slice(0, -1));
+                movieObj.title = $(this).children("td").children("a").text();
+                movieObj.link =  "http://www.rottentomatoes.com" + $(this).children("td").children("a").attr('href');
+                movieObj.year = $(this).children().eq(-1).text();
+                data.push(movieObj);
+            }
+        });
+        // Reverse data so it is in chronological order
+        data = data.reverse();
+        // Add film # to the data
+        for (var i = 0; i<data.length; i++){
+            data[i].x = i+1;
+        }
+    });
+
+    return data;
 }
 
 function getLinearRegression(data){
@@ -85,7 +110,9 @@ function getLinearRegression(data){
     data.forEach(function(elem){
         resultArr.push([elem.x, elem.y]);
     });
-    const result = regression.linear(resultArr);
+    var result = regression.linear(resultArr);
+    // min/max decimation for performance improvement
+    result = [result.points[0], result.points[result.points.length-1]];
     return result;
 }
 
