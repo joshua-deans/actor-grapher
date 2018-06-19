93e5edef9cae781a5c472a17d9c61eba2e434e1b
jdeans <joshua.deans21@gmail.com>
2018-06-18 22:23:21 -0700

    Refactored the files
    I refactored the program, so by extracting methods, and making my code more readable.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var externalFunc = require("./functions/index.js");
 var session = require('express-session');
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use("/public", express.static(__dirname + "/public"));
 app.use(session({
     secret: "Anything I want",
@@ -18,11 +18,11 @@ app.use(flash());
 
 app.set("view engine", "ejs");
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('index', {error: []});
 });
 
-app.post('/', function(req, res){
+app.post('/', function (req, res) {
     var name = req.body.actorName.trim();
     var nameSplit = name.split(" ");
     var urls = externalFunc.getUrl(nameSplit);
@@ -32,6 +32,6 @@ app.post('/', function(req, res){
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP;
 
-app.listen(port, ip, function(){
-    console.log( "Listening on " + ip + ", port " + port )
+app.listen(port, ip, function () {
+    console.log("Listening on " + ip + ", port " + port)
 });
--- functions/index.js
+++ functions/index.js
@@ -4,22 +4,26 @@ const regression = require('regression');
 const Promise = require('promise');
 var funcObj = {};
 
-funcObj.getUrl = function(name){
-    for (var i = 0; i < name.length; i++){
-        name[i] = name[i].toLowerCase();
-        if (name[i].includes("-")){
-            name[i].replace("-", "");
-        }
+function removeDashes(name, i) {
+    name[i] = name[i].toLowerCase();
+    if (name[i].includes("-")) {
+        name[i].replace("-", "");
     }
-    if (name[1] === "dicaprio"){
+}
+
+funcObj.getUrl = function (name) {
+    for (let i = 0; i < name.length; i++) {
+        removeDashes(name, i);
+    }
+    if (name[1] === "dicaprio") {
         name[1] = "di";
         name[2] = "caprio";
     }
-    var actorUrl1 = name.join("_");
-    while (actorUrl1.includes(".")){
-        actorUrl1 = actorUrl1.replace(".","");
+    let actorUrl1 = name.join("_");
+    while (actorUrl1.includes(".")) {
+        actorUrl1 = actorUrl1.replace(".", "");
     }
-    var actorUrl2 = actorUrl1.replace("_", "-");
+    let actorUrl2 = actorUrl1.replace("_", "-");
 
     actorUrl1 = 'https://www.rottentomatoes.com/celebrity/' + actorUrl1;
     actorUrl2 = 'https://www.rottentomatoes.com/celebrity/' + actorUrl2;
@@ -29,18 +33,18 @@ funcObj.getUrl = function(name){
     return [actorUrl1, actorUrl2, name];
 };
 
-funcObj.getWebData = function(urlArr, req, res) {
+funcObj.getWebData = function (urlArr, req, res) {
     var url = urlArr[0];
     var name = urlArr[2];
     // scrape the first url
     webScrape(url, name, urlArr, req, res);
 };
 
-function webScrape(url, name, urlArr, req, res){
+function webScrape(url, name, urlArr, req, res) {
     var data = [];
-    request(url, function(error, response, html){
+    request(url, function (error, response, html) {
         // First we'll check to make sure no errors occurred when making the request
-        if(!error){
+        if (!error) {
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
             var $ = cheerio.load(html);
             // Finally, we'll define the variables we're going to capture
@@ -51,11 +55,11 @@ function webScrape(url, name, urlArr, req, res){
             // if data is not empty
             if (data.length > 0) {
                 var linearData = getLinearRegression(data);
-                res.render('graph', {actorName: actorName, dataArr: data, linearData: linearData});
+                return res.render('graph', {actorName: actorName, dataArr: data, linearData: linearData});
             }
             else {
                 // in case of no data being returned
-                if (url === urlArr[1]){
+                if (url === urlArr[1]) {
                     console.log(name + " was not found on Rotten Tomatoes");
                     req.flash("error", "\"" + name + "\" was not found on Rotten Tomatoes");
                     return res.render("index", {error: req.flash("error")});
@@ -66,53 +70,55 @@ function webScrape(url, name, urlArr, req, res){
             }
         }
         else {
-            if (url === urlArr[1]){
-                console.log("An error occured when finding " + name);
+            if (url === urlArr[1]) {
+                console.log("An error occurred when finding " + name);
                 req.flash("error", "An error occurred when finding " + name);
                 return res.render("index", {error: req.flash("error")});
             }
-            else {
-                webScrape(urlArr[1], name, urlArr, req, res);
-            }
+            webScrape(urlArr[1], name, urlArr, req, res);
         }
     });
 }
 
-function cheerioMethod($, data){
-    $('#filmographyTbl').filter(function(){
+function createMovieObj(searchData, $, data) {
+    searchData.each(function (i, elem) {
+        var movieObj = {};
+        var score = $(this).children("td").children(".tMeterIcon").children(".tMeterScore").text();
+        if (score && score !== "No Score Yet") {
+            movieObj.y = Number(score.slice(0, -1));
+            movieObj.title = $(this).children("td").children("a").text();
+            movieObj.link = "http://www.rottentomatoes.com" + $(this).children("td").children("a").attr('href');
+            movieObj.year = $(this).children().eq(-1).text();
+            data.push(movieObj);
+        }
+    });
+}
+
+function cheerioMethod($, data) {
+    $('#filmographyTbl').filter(function () {
         // Let's store the data we filter into a variable so we can easily see what's going on.
         var searchData = $(this).children("tbody").children();
         // Go through each film and collect data
-        searchData.each(function(i, elem) {
-            var movieObj = {};
-            var score = $(this).children("td").children(".tMeterIcon").children(".tMeterScore").text();
-            if (score && score !== "No Score Yet") {
-                movieObj.y = Number(score.slice(0, -1));
-                movieObj.title = $(this).children("td").children("a").text();
-                movieObj.link =  "http://www.rottentomatoes.com" + $(this).children("td").children("a").attr('href');
-                movieObj.year = $(this).children().eq(-1).text();
-                data.push(movieObj);
-            }
-        });
+        createMovieObj(searchData, $, data);
         // Reverse data so it is in chronological order
         data = data.reverse();
         // Add film # to the data
-        for (var i = 0; i<data.length; i++){
-            data[i].x = i+1;
+        for (var i = 0; i < data.length; i++) {
+            data[i].x = i + 1;
         }
     });
 
     return data;
 }
 
-function getLinearRegression(data){
+function getLinearRegression(data) {
     var resultArr = [];
-    data.forEach(function(elem){
+    data.forEach(function (elem) {
         resultArr.push([elem.x, elem.y]);
     });
     var result = regression.linear(resultArr);
     // min/max decimation for performance improvement
-    result = [result.points[0], result.points[result.points.length-1]];
+    result = [result.points[0], result.points[result.points.length - 1]];
     return result;
 }
 
